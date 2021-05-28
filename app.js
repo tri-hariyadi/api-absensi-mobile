a487d37a8509e428fa14aa97bccbdeb03d076396
trihariyadi <trihariyadi24@gmail.com>
2021-05-28 09:25:01 +0700

    [Modify] multer

--- app.js
+++ app.js
@@ -9,8 +9,6 @@ var corsOptions = {
   origin: "http://localhost:8081"
 };
 
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use(cors(corsOptions));
